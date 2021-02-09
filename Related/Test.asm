5fc168680f7d582fd43057eeb1f83d5d6657f675
efeemir103 <efeemir103@gmail.com>
2021-02-09 06:06:07 +0300

    @Nycrera check branching out

--- Related/Test.asm
+++ Related/Test.asm
@@ -1,3 +1,4 @@
+; Load button data:
 LD R1, 0x0
 
 MOV R2, 2
@@ -10,9 +11,14 @@ ADD R1, R1, R3
 ; START Multiplication LOOP:
 ADD R4, R4, R2
 DEC R1, R3
+
+; debug
+; STR R1, 0x1
+
 CMP R1, R5
 JNE 5
 ; END Multiplication LOOP;
 
+; Write to digital tube:
 STR R4, 0x1
 RES
